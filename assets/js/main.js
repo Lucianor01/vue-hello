73d6441fed4e17c5badee07015275319f70c97d9
Lucianor01 <lucianorosario02@gmail.com>
2023-04-18 14:34:50 +0200

    Esercizio-completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: "Hello Vue!",
+            image: "https://picsum.photos/seed/picsum/300/300",
+            altImage: "immagine"
         }
     }
 }).mount('#app')
